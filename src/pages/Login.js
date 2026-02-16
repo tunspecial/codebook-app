b6035a8c5c3924df6f1d21bfa68e2234d807bb20
KTT <tunspecial@gmail.com>
2026-02-16 20:01:44 +0630

    update Guest

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -29,23 +29,23 @@ export const Login = () => {
         }
     }
 
-    // const handleGuestLogin = async () => {
+    const handleGuestLogin = async () => {
 
-    //     email.current.value = process.env.REACT_APP_GUEST_LOGIN
-    //     password.current.value = process.env.REACT_APP_GUEST_PASSWORD
+        email.current.value = process.env.REACT_APP_GUEST_LOGIN
+        password.current.value = process.env.REACT_APP_GUEST_PASSWORD
 
-    //     try {
-    //         const authDetails = {
-    //             email: email.current.value,
-    //             password: password.current.value
-    //         }
-    //         const data = await logIn(authDetails)
-    //         data.accessToken ? navigate("/products") : toast.error(data)
-    //     }
-    //     catch (error) {
-    //         toast.error(error.message, { closeButton: true, position: "bottom-center" })
-    //     }
-    // }
+        try {
+            const authDetails = {
+                email: email.current.value,
+                password: password.current.value
+            }
+            const data = await logIn(authDetails)
+            data.accessToken ? navigate("/products") : toast.error(data)
+        }
+        catch (error) {
+            toast.error(error.message, { closeButton: true, position: "bottom-center" })
+        }
+    }
 
 
     return (
@@ -64,7 +64,7 @@ export const Login = () => {
                 </div>
                 <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Log In</button>
             </form>
-            {/* <button className="mt-3 cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login As Guest</button> */}
+            <button onClick={handleGuestLogin} className="mt-3 cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login As Guest</button>
         </main>
     )
 }
